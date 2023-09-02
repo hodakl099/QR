921fc6ae6262dced1f2f5016ae4bbbb7846d94c8
mahmoud <mahmoud.alkateb22@gmail.com>
2023-09-02 13:24:09 +0200

    connected the website to the server end point

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Button, Img, Line, RatingBar, Text } from "components";
 import Footer from "components/Footer";
 import { useTranslation } from 'react-i18next';
-import { Category } from './models';
-import { fetchCategoriesByRestaurant } from "models/api";
+import { Category } from './models/models';
+import { fetchCategoriesByRestaurant } from "./models/api";
 
 const MenuPage = () => {
   const navigate = useNavigate();
@@ -83,88 +83,57 @@ const MenuPage = () => {
               Our Popular Menu
             </Text>
             <div className="flex flex-col font-poppins gap-[50px] items-center justify-start w-full">
-              <div className="flex sm:flex-col flex-row gap-7 items-center justify-between rounded-[16px] w-full">
-                <Button className="bg-red-400 cursor-pointer font-semibold min-w-[232px] py-[26px] rounded-[16px] text-center text-white-A700 text-xl">
-                  All catagory
-                </Button>
-                <Button className="bg-gray-400_63 cursor-pointer min-w-[192px] py-[27px] rounded-[16px] text-center text-gray-900 text-xl">
-                  Dinner
-                </Button>
-                <Button className="bg-gray-400_63 cursor-pointer min-w-[192px] py-[27px] rounded-[16px] text-center text-gray-900 text-xl">
-                  Lunch
-                </Button>
-                <Button className="bg-gray-400_63 cursor-pointer min-w-[192px] py-[27px] rounded-[16px] text-center text-gray-900 text-xl">
-                  Dessert
-                </Button>
-                <Button className="bg-gray-400_63 cursor-pointer min-w-[192px] py-[27px] rounded-[16px] text-center text-gray-900 text-xl">
-                  Drink
-                </Button>
-              </div>
               <div className="flex flex-col gap-12 items-center justify-start w-full">
                 <div className="flex flex-col items-center justify-start rounded-[40px] w-full">
                   <div className="md:gap-5 gap-[35px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center min-h-[auto] w-full">
-                     <div className="flex flex-col font-poppins gap-[50px] items-center justify-start w-full">
-        <div className="flex sm:flex-col flex-row gap-7 items-center justify-between rounded-[16px] w-full">
-          {/* Dynamically render category buttons */}
-          {categories.map((category) => (
-            <Button
-              key={category.id}
-              className="bg-gray-400_63 cursor-pointer min-w-[192px] py-[27px] rounded-[16px] text-center text-gray-900 text-xl"
-            >
-              {category.name}
-            </Button>
-          ))}
+                  {categories.map((category) => (
+    <div className="bg-white-A700 flex flex-1 flex-col gap-6 items-center justify-center p-[30px] sm:px-5 rounded-[40px] w-full" key={category.id}>
+      <Img
+        className="h-[270px] md:h-auto mt-1.5 object-cover w-[270px]"
+        src={category.imageUrl}
+        alt={category.name}
+      />
+      <div className="flex flex-col items-center justify-end mb-1.5 pt-[17px] w-full">
+        <div className="flex flex-col gap-[18px] items-center justify-start w-full">
+          <Text
+            className="text-3xl sm:text-[26px] md:text-[28px] text-gray-900"
+            size="txtPoppinsSemiBold30"
+          >
+            {category.name}
+          </Text>
+          <Text
+            className="leading-[200.00%] text-center text-gray-800 text-sm w-full"
+            size="txtPoppinsRegular14"
+          >
+            {/* Add description here if you have it */}
+          </Text>
         </div>
-        {/* ...Existing Code for displaying menu items... */}
+        <div className="flex flex-row items-center justify-center mt-3.5 rounded-[1.76px] w-[49%] md:w-full">
+          <RatingBar
+            className="flex justify-between w-[140px]"
+            value={5} // Replace with dynamic value if available
+            starCount={5}
+            activeColor="#f54748"
+            size={24}
+          />
+        </div>
+        <div className="flex flex-row gap-[34px] items-center justify-between mt-[30px] rounded-lg w-[95%] md:w-full">
+          <Text
+            className="sm:text-[21px] md:text-[23px] text-[25px] text-gray-900"
+            size="txtPoppinsSemiBold25"
+          >
+            ${category.price} 
+          </Text>
+          <Button
+            className="common-pointer bg-red-400 cursor-pointer font-semibold min-w-[158px] py-[19px] rounded-lg text-base text-center text-white-A700"
+            onClick={() => navigate("/checkout")}
+          >
+            Order now
+          </Button>
+        </div>
       </div>
-                    <div className="bg-white-A700 flex flex-1 flex-col gap-6 items-center justify-center p-[30px] sm:px-5 rounded-[40px] w-full">
-                      <Img
-                        className="h-[270px] md:h-auto mt-1.5 object-cover w-[270px]"
-                        src="images/img_pngitem41084.png"
-                        alt="PngItem41084"
-                      />
-                      <div className="flex flex-col items-center justify-end mb-1.5 pt-[17px] w-full">
-                        <div className="flex flex-col gap-[18px] items-center justify-start w-full">
-                          <Text
-                            className="text-3xl sm:text-[26px] md:text-[28px] text-gray-900"
-                            size="txtPoppinsSemiBold30"
-                          >
-                            Spaghetti
-                          </Text>
-                          <Text
-                            className="leading-[200.00%] text-center text-gray-800 text-sm w-full"
-                            size="txtPoppinsRegular14"
-                          >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Egestas consequat mi eget auctor aliquam,
-                            diam.{" "}
-                          </Text>
-                        </div>
-                        <div className="flex flex-row items-center justify-center mt-3.5 rounded-[1.76px] w-[49%] md:w-full">
-                          <RatingBar
-                            className="flex justify-between w-[140px]"
-                            value={5}
-                            starCount={5}
-                            activeColor="#f54748"
-                            size={24}
-                          ></RatingBar>
-                        </div>
-                        <div className="flex flex-row gap-[34px] items-center justify-between mt-[30px] rounded-lg w-[95%] md:w-full">
-                          <Text
-                            className="sm:text-[21px] md:text-[23px] text-[25px] text-gray-900"
-                            size="txtPoppinsSemiBold25"
-                          >
-                            $12.05
-                          </Text>
-                          <Button
-                            className="common-pointer bg-red-400 cursor-pointer font-semibold min-w-[158px] py-[19px] rounded-lg text-base text-center text-white-A700"
-                            onClick={() => navigate("/checkout")}
-                          >
-                            Order now
-                          </Button>
-                        </div>
-                      </div>
-                    </div>
+    </div>
+  ))}
                     <div className="bg-white-A700 flex flex-1 flex-col gap-6 items-center justify-center p-[30px] sm:px-5 rounded-[40px] w-full">
                       <Img
                         className="h-[270px] md:h-auto mt-1.5 object-cover w-[270px]"
